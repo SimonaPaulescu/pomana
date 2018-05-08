1fdea144df05c309b8e86e228e4fdce0364880d6
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2018-05-08 12:22:09 +0300

    Fix spacing

--- graph_search.c
+++ graph_search.c
@@ -5,17 +5,19 @@
 int full(int *array, int size) {
 
 	int i, count = 0;
+
 	//printf("Elements are \n");
-	for(i = 1; i < size; i++) {
+	for (i = 1; i < size; i++) {
 		//printf("%d\n", array[i]);
-		if(array[i] == 0) {
+		if (array[i] == 0) {
 			return 0;
 		}
 	}
+
 	return 1;
 }
 
-void dijkstra(Graph *graph, int start ) {
+void dijkstra(Graph *graph, int start) {
 
 	int size = graph->MaxSize;
 	int *distance, distanceVal = 1, current = start, minIndex, minVal, i;
@@ -24,24 +26,25 @@ void dijkstra(Graph *graph, int start ) {
 	distance = calloc(size - 1, sizeof(int));
 	visitedNodes = calloc(size - 1, sizeof(int));
 
-	while(!full(visitedNodes, size)) {
+	while (!full(visitedNodes, size)) {
 		List *adjacency;
 		visitedNodes[current] = 1;
 		adjacency = graph->table[current].outlist;
 		printf("Adjacency is\n");
-		while(adjacency != NULL) {
+		while (adjacency != NULL) {
 			printf("%d\n", adjacency->index);
-			if(distance[adjacency->index] > distanceVal && distance[adjacency->index] != 0) {
+			if (distance[adjacency->index] > distanceVal && distance[adjacency->index] != 0) {
 				distance[adjacency->index] = distanceVal;
-			} else if(distance[adjacency->index] == 0) {
+			} else if (distance[adjacency->index] == 0) {
 				distance[adjacency->index] = distanceVal;
 			}
 			adjacency = adjacency->next;
 		}
+
 		i = 1;
 		//we find an initial minimum value
-		while(i < size) {
-			if(distance[i] != 0 && visitedNodes[i] == 0){
+		while (i < size) {
+			if (distance[i] != 0 && visitedNodes[i] == 0) {
 				printf("distance : %d\n", distance[i]);
 				minVal = distance[i];
 				minIndex = i;
@@ -49,15 +52,17 @@ void dijkstra(Graph *graph, int start ) {
 			}
 			i++;
 		}
+
 		//now we loop again to find the minimum
-		for(i = 1; i < size; i++) {
-			if(visitedNodes[i] == 0 && distance[i] != 0 ) {
-				if(distance[i] <= minVal) {
+		for (i = 1; i < size; i++) {
+			if (visitedNodes[i] == 0 && distance[i] != 0) {
+				if (distance[i] <= minVal) {
 					minVal = distance[i];
 					minIndex = i;
 				}
 			}
 		}
+
 		printf("Min is %d with val %d\n", minIndex, minVal);
 		current = minIndex;
 		printf("Current is %d\n", current);
@@ -65,8 +70,8 @@ void dijkstra(Graph *graph, int start ) {
 	}
 
 	printf("Distances from %d are\n", start);
-	for(i = 1; i < size; i++) {
-		if(i != start) {
+	for (i = 1; i < size; i++) {
+		if (i != start) {
 			printf("%d , %d\n", i, distance[i]);
 		}
 	}
@@ -75,38 +80,44 @@ void dijkstra(Graph *graph, int start ) {
 int checkList(int target, Node node) {
 
 	List *list = node.outlist;
-	while(list != NULL) {
-		if(list->index == node.index) {
+
+	while (list != NULL) {
+		if (list->index == node.index) {
 			return 0;
 		}
 		list = list->next;
 	}
+
 	return 1;
 }
 
 
-Node findMax(List *list, Graph *graph){
+Node findMax(List *list, Graph *graph) {
 
 	int max = 0;
 	Node node;
-	while(list != NULL) {
-		if(graph->table[list->index].outdegree > max) {
+
+	while (list != NULL) {
+		if (graph->table[list->index].outdegree > max) {
 			max = graph->table[list->index].outdegree;
 			node = graph->table[list->index];
 		}
 		list = list->next;
 	}
+
 	return node;
 }
 
 void heuristicSearch(Graph *graph, int target) {
 
 	Node current = graph->table[1];
-	printf("First node is %d\n", current.index);
-	Node *path = (Node*)malloc(graph->MaxSize * sizeof(Node));
-	int i = 1;
+	Node *path = (Node *) malloc(graph->MaxSize * sizeof(Node));
 	List *adjacency = current.outlist;
-	while(checkList(target, current) && adjacency != NULL) {
+	int i = 1;
+
+	printf("First node is %d\n", current.index);
+
+	while (checkList(target, current) && adjacency != NULL) {
 		path[i] = current;
 		//printf("Path is %d\n", current.index);
 		i++;
@@ -114,8 +125,8 @@ void heuristicSearch(Graph *graph, int target) {
 		adjacency = current.outlist;
 	}
 	path[i] = current;
+
 	printf("Shortest path\n");
 	for (i = 1; i < graph->MaxSize; i++)
 		printf("%d\n", path[i].index);
-
 }
