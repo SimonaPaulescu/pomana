1b103110ecb1824b46c57212b92413b4aca9e639
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2018-05-10 19:27:20 +0300

    Make heuristicSearch() work again

--- graph_search.c
+++ graph_search.c
@@ -77,60 +77,110 @@ void dijkstra(Graph *graph, int start) {
 	}
 }
 
-int checkList(int target, Node node) {
+/*
+ * Check if node index (index parameter) is adjacent to node. Look
+ * through node adjacency list; return 1 (true) if node index is part
+ * of the adjanceny list and 0 (false) otherwise.
+ */
 
-	List *list = node.outlist;
+int isNodeIndexAdjacent(Node *node, int index) {
 
+	List *list = node->outlist;
+	/* Check if current node is the index node. */
+	if (node->index == index)
+		return 1;
+
+	/* Check if index node is in adjacency list. */
 	while (list != NULL) {
-		if (list->index == node.index) {
-			return 0;
-		}
+		if (list->index == index)
+			return 1;
 		list = list->next;
 	}
 
-	return 1;
+	return 0;
 }
 
 
-Node findMax(List *list, Graph *graph) {
+/*
+ * Find the next unvisited node with the maximum out degree.
+ */
 
+Node *findMaxOutDegreeNode(Node *node, Graph *graph) {
+
+	List *list = node->outlist;
 	int max = 0;
-	Node node;
+	Node *current = NULL;
 
 	while (list != NULL) {
-		if (graph->table[list->index].outdegree > max) {
+		/*
+		 * Check if node in adjancecy list is not current node.
+		 * Select node with maximum outdegree from the rest
+		 */
+		if (list->index != node->index && graph->table[list->index].state == 0 && graph->table[list->index].outdegree > max) {
 			max = graph->table[list->index].outdegree;
-			node = graph->table[list->index];
+			current = &graph->table[list->index];
 		}
 		list = list->next;
 	}
 
-	return node;
+	return current;
 }
+
+
+/*
+ * Mark all graph nodes as unvisited.
+ */
+
+void markNodesAsUnvisited(Graph *graph) {
+
+	int i;
+
+	for (i = 1; i <= graph->MaxSize; i++)
+		graph->table[i].state = 0;
+}
+
 
 void heuristicSearch(Graph *graph, int target) {
 
-	Node current = graph->table[1];
-	Node *path = (Node *) malloc(graph->MaxSize * sizeof(Node));
+	Node *current = &graph->table[1];
+	Node **path = (Node **) malloc(graph->MaxSize * sizeof(Node *));
 	int i = 0;
 	int path_len = 0;
 
-	//printf("First node is %d\n", current.index);
+	/*
+	printf("First node is %d\n", current->index);
+	printf("current->outlist %p\n", current->outlist);
+	printf("isNodeIndexAdjacent(%d, %d): %d\n", current->index, target, isNodeIndexAdjacent(current, target));
+	*/
 
-	while (checkList(target, current) && current.outlist != NULL) {
+	while (!isNodeIndexAdjacent(current, target)) {
 		path[path_len] = current;
-		//printf("Path is %d\n", current.index);
+		//printf("Path is %d\n", current->index);
 		path_len++;
-		current = findMax(current.outlist, graph);
+		current->state = 1;
+		current = findMaxOutDegreeNode(current, graph);
+		if (current == NULL)
+			break;
 	}
 	path[path_len] = current;
 	path_len++;
 
-	printf("Shortest path from node %d to %d: [ ", 1, target);
-	for (i = 0; i < path_len; i++) {
-		if (path[i].index == 0)
-			break;
-		printf("%d ", path[i].index);
+	if (current != NULL && isNodeIndexAdjacent(current, target)) {
+		if (current->index != target) {
+			path[path_len] = &graph->table[target];
+			path_len++;
+		}
+		printf("Shortest path from node %d to %d: [ ", 1, target);
+		for (i = 0; i < path_len; i++) {
+			if (path[i]->index == 0)
+				break;
+			printf("%d ", path[i]->index);
+		}
+		printf("]\n");
 	}
-	printf("]\n");
+	else
+		printf("No path from node %d to %d.\n", 1, target);
+
+	free(path);
+	markNodesAsUnvisited(graph);
 }
